edb7abb3fa94200c43fc7089b13e01f1094e6769
Patrick Ogonyo Ochanyo <93025209+Nyakwar-Orera@users.noreply.github.com>
2025-07-24 17:02:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-key")
     DATA_FILE = os.getenv("DATA_FILE_PATH", "data/browsing_history_data_cleaned.csv")
     TIME_RECORDS_FILE = os.getenv("TIME_RECORDS_PATH", "data/time_records_sample.txt")
-    GOOGLE_DRIVE_FILE_ID = os.getenv("GOOGLE_DRIVE_FILE_ID")  # <-- added this line
+    GOOGLE_DRIVE_FILE_ID = os.getenv("GOOGLE_DRIVE_FILE_ID")
